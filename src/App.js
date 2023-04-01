8e0e7812b1eba23232b700546caea367d1036938
Rajan Chaudhary <rajann44@gmail.com>
2023-04-01 15:27:13 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 import "./App.css";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
